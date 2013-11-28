2aab056fd947a3accdf8ed177c53abcc75c51511
Jordan Rennert <jbr7eq@virginia.edu>
2013-11-28 16:42:58 -0500

    finished XmlBuilder class, still need to make XmlReader / look into existing XML readers

--- 4240Project/src/cs4240f13/hoowhatyouwearing/utility/XmlBuilder.java
+++ 4240Project/src/cs4240f13/hoowhatyouwearing/utility/XmlBuilder.java
@@ -1,5 +1,6 @@
 package cs4240f13.hoowhatyouwearing.utility;
 import java.io.File;
+import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -10,12 +11,17 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 
-public class XmlBuilder {
+public class XmlBuilder { 
 	
+	/* Public Methods */
 	/**
 	 * Creates a new XML file with the settings params if not already in application files.
 	 * @return true if no exception
@@ -24,49 +30,95 @@ public class XmlBuilder {
 		File file = new File("./settings.xml"); // config file URL info goes here
 		if (file.isFile()) return true; // check to see if there is a config file
 		
-		try {
-			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-			
-			// root elements
-			Document doc = docBuilder.newDocument();
-			Element root = doc.createElement("settings");
-			doc.appendChild(root);
-			
-			// second level elements
-			Element unit = doc.createElement("unit");
-			root.appendChild(unit);
-			
-			Element location = doc.createElement("location");
-			root.appendChild(location);
-			
-			Element clothes = doc.createElement("clothes");
-			root.appendChild(clothes);
-			
-			// write the content into xml file
-			TransformerFactory transformerFactory = TransformerFactory.newInstance();
-			Transformer transformer = transformerFactory.newTransformer();
-			DOMSource source = new DOMSource(doc);
-			StreamResult result = new StreamResult(file);
-	 
-			transformer.transform(source, result);
-		}
-		catch (ParserConfigurationException e) {
-			e.printStackTrace();
-			return false;
-		}
-		catch (TransformerException e) {
-			e.printStackTrace();
-			return false;
-		}
+		// root elements
+		Document doc = generateDoc(null);
+		Element root = doc.createElement("settings");
+		doc.appendChild(root);
+		
+		// second level elements
+		Element unit = doc.createElement("unit");
+		root.appendChild(unit);
+		
+		Element location = doc.createElement("location");
+		root.appendChild(location);
+		
+		Element clothes = doc.createElement("clothes");
+		root.appendChild(clothes);
+		
+		// write the content into xml file
+		writeToFile(doc, file);
 		
 		return true;
 	}
 	
-	public static void main(String[] args) {
-		File file = new File("src/ApiRequest.java");
-		System.out.println(file.isFile());
-		createIfMissing();
+	public static boolean addElement(String parent, String tag, String value) {
+		String filePath = "./settings.xml";
+		Document doc = generateDoc(filePath);
+		
+		Node parentNode = doc.getElementsByTagName(parent).item(0);
+		Element childNode = doc.createElement(tag);
+		childNode.appendChild(doc.createTextNode(value));
+		parentNode.appendChild(childNode);
+		
+		writeToFile(doc, new File(filePath));
+		return true;
+	}
+	
+	public static boolean removeElement(String parent, String tag, String value) {
+		String filePath = "./settings.xml";
+		Document doc = generateDoc(filePath);
+		
+		Node parentNode = doc.getElementsByTagName(parent).item(0);
+		NodeList children = parentNode.getChildNodes();
+		Node removedNode = null;
+		
+		for (int i = 0; i < children.getLength(); i++) {
+			Node tempNode = children.item(i);
+			if (tempNode.getTextContent().equals(value)) {
+				removedNode = tempNode;
+				break;
+			}
+		}
+		
+		if (removedNode == null) {
+			return false;
+		}
+		
+		parentNode.removeChild(removedNode);
+		writeToFile(doc, new File(filePath));
+		return true;
+	}
+	
+	/* Helper Methods */
+	private static Document generateDoc(String filePath) {
+		try {
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+			return (filePath == null) ? docBuilder.newDocument() : docBuilder.parse(filePath);
+		}
+		catch (SAXException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private static void writeToFile(Document doc, File file) {
+		try {
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(file);
+			transformer.transform(source, result);
+		}
+		catch (TransformerException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
