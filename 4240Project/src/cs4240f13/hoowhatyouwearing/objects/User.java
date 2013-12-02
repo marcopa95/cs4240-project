b55af77c398c9bf4eb1f9a51368b6715bcdb0585
Jordan Rennert <jbr7eq@virginia.edu>
2013-12-01 21:18:43 -0500

    readded a few files...too lazy to write formal commit

--- 4240Project/src/cs4240f13/hoowhatyouwearing/objects/User.java
+++ 4240Project/src/cs4240f13/hoowhatyouwearing/objects/User.java
@@ -1,10 +1,15 @@
 package cs4240f13.hoowhatyouwearing.objects;
 
+import java.util.List;
+
+import cs4240f13.hoowhatyouwearing.utility.TempUnit;
 import cs4240f13.hoowhatyouwearing.utility.XmlBuilder;
 
 public class User {
 	private static final User CONTEXT = new User();
-	
+	private TempUnit temperature;
+	private String location;
+	private List clothes;
 	
 	private User() {
 		
---
+++ 4240Project/src/cs4240f13/hoowhatyouwearing/utility/XmlReader.java
@@ -0,0 +1,11 @@
+package cs4240f13.hoowhatyouwearing.utility;
+
+import java.util.ArrayList;
+
+public class XmlReader {
+	
+	public static ArrayList<String> retrieveElements(String parentTag, String childTag) {
+		return null; //working on this now
+	}
+	
+}
